8193d5d8f392b59b372e6f8694f3033e22ff06e1
Michael Scott Riccardi <mike.computerwiz@gmail.com>
2018-02-27 20:24:34 +0000

    first

---
+++ stats_data.php
@@ -0,0 +1,7 @@
+<?php 
+    header('Content-type: application/json');
+
+    $data = '{"firstEndDate":"2015-07-29T14:00:00.236Z","invoicesStart":1042960,"purchasesStart":1.1833017492711487E9,"secondEndDate":"2015-07-29T15:00:00.236Z","invoicesEnd":1043960,"purchasesEnd":1.1836027492711487E9}';
+    
+    echo $data;
+?>
